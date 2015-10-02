f86730b8381f192173927b69f3addf58db5b64ea
Rafael S. de Souza <rafael.2706@gmail.com>
2015-10-02 12:49:44 +0200

    rearrange folders

--- script /Logit_AGN_gal.R
+++ script /Logit_AGN_gal.R
@@ -4,7 +4,7 @@ library(pROC)
 require(plyr)
 require(gam)
 library(glmnet)
-data<-read.csv("sample_agn.csv",header=TRUE,na.strings="")
+data<-read.csv("..//data/Logit_sample-2.csv",header=TRUE,na.strings="")
 data2<-na.omit(data)
 data2<-data2[data2$logMstar>0,]
 data2<-data2[which(data2$vlos_sigma!=Inf),]
@@ -98,13 +98,13 @@ dev.off()
 
 market.size <- 800
 icecream$opportunity <- market.size - icecream$units
-bin.glm <- glm(cbind(units, opportunity) ~ temp, data=icecream, 
+bin.glm <- glm(cbind(units, opportunity) ~ temp, data=icecream,
                family=binomial(link = "logit"))
 par(mfrow=c(2,2))
 plot(bin.glm)
 title(outer=TRUE, line = -1,
-      main = list("Binomial (logit) GLM", 
-                  cex=1.25,col="black", font=2)) 
+      main = list("Binomial (logit) GLM",
+                  cex=1.25,col="black", font=2))
 
 meanProb <- predict(bin.glm, type="response")
 meanPred <- meanProb*market.size
@@ -125,12 +125,12 @@ plotData <- lapply(
 
 
 icecream <- data.frame(
-  temp=c(11.9, 14.2, 15.2, 16.4, 17.2, 18.1, 
+  temp=c(11.9, 14.2, 15.2, 16.4, 17.2, 18.1,
          18.5, 19.4, 22.1, 22.6, 23.4, 25.1),
-  units=c(185L, 215L, 332L, 325L, 408L, 421L, 
+  units=c(185L, 215L, 332L, 325L, 408L, 421L,
           406L, 412L, 522L, 445L, 544L, 614L)
 )
-glmModelPlot <- function(x, y, xlim,ylim, meanPred,  LwPred, UpPred, 
+glmModelPlot <- function(x, y, xlim,ylim, meanPred,  LwPred, UpPred,
                          plotData, main=NULL){
   ## Based on code by Arthur Charpentier:
   ## http://freakonometrics.hypotheses.org/9593
@@ -139,7 +139,7 @@ glmModelPlot <- function(x, y, xlim,ylim, meanPred,  LwPred, UpPred,
   N <- length(meanPred)
   zMax <- max(unlist(sapply(plotData, "[[", "z")))*1.5
   mat <- persp(xlim, ylim, matrix(0, n, n), main=main,
-               zlim=c(0, zMax), theta=-30, 
+               zlim=c(0, zMax), theta=-30,
                ticktype="detailed",box=FALSE)
   C <- trans3d(x, UpPred, rep(0, N),mat)
   lines(C, lty=2)
@@ -168,13 +168,13 @@ glmModelPlot <- function(x, y, xlim,ylim, meanPred,  LwPred, UpPred,
 
 market.size <- 800
 icecream$opportunity <- market.size - icecream$units
-bin.glm <- glm(cbind(units, opportunity) ~ temp, data=icecream, 
+bin.glm <- glm(cbind(units, opportunity) ~ temp, data=icecream,
                family=binomial(link = "logit"))
 par(mfrow=c(2,2))
 plot(bin.glm)
 title(outer=TRUE, line = -1,
-      main = list("Binomial (logit) GLM", 
-                  cex=1.25,col="black", font=2)) 
+      main = list("Binomial (logit) GLM",
+                  cex=1.25,col="black", font=2))
 
 meanProb <- predict(bin.glm, type="response")
 meanPred <- meanProb*market.size
