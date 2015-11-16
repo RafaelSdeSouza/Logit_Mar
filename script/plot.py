f13aa882776c099cc1f4f9485a9757904ddbbd76
Miguel de Val-Borro <miguel@archlinux.net>
2015-11-16 17:05:04 -0500

    use seaborn package

--- script/plot.py
+++ script/plot.py
@@ -3,7 +3,10 @@
 import pickle
 import corner
 import argparse
-from os.path import splitext
+from os.path import abspath, splitext, join
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Parsing command line arguments
 parser = argparse.ArgumentParser()
@@ -25,3 +28,12 @@ figure = corner.corner(samples[:,:,:].reshape(ndims[0] * ndims[1], ndims[2]),
                        truths=truths,
                        show_titles=True)
 figure.savefig("{0}.png".format(splitext(args.filename)[0]))
+plt.close()
+
+# read data into pandas data frame
+datadir = abspath('../data')
+datafile = join(datadir, "matched.txt")
+df = pd.read_csv(datafile, sep=" *")
+
+sns.violinplot(y="color_gr", hue="bpt", data=df, split=True)
+plt.savefig("output.png")
