978085b85a216047fb11d49acb1bd0bcfb70fbe4
Rafael <rafael.2706@gmail.com>
2015-12-02 07:37:09 +0100

    gam

--- script/diagnostic.R
+++ script/diagnostic.R
@@ -13,27 +13,22 @@ data_cut   <- subset(data_cut, zoo == "E")
 
 
 mod1<- glm(bpt ~ logM200_L+RprojLW_Rvir, family=binomial("logit"),data=data_cut)
-mod2<- gam(bpt ~ s(RprojLW_Rvir)+s(logM200_L), family=binomial("logit"),data=data_cut)
+mod2<- gam(bpt ~ s(RprojLW_Rvir,3)+s(logM200_L,3), family=binomial("logit"),data=data_cut)
+plot(mod2,pages=1,residuals=TRUE)
 
-
-plot(fitted(mod2), jitter(data_cut$bpt, amount=0.05), xlab="Fitted values",
-                      ylab="Probability of presence", las=1, cex.lab=1.2, cex=0.8)
-abline(0,1, lty=3)
-t.breaks <-cut(fitted(mod2), seq(0,1, by=0.1))
+# GoF Visual 
+t.breaks <-cut(fitted(mod1), seq(0,1, by=0.1))
 means <-tapply(data_cut$bpt, t.breaks, mean)
 semean <-function(x) sd(x)/sqrt(length(x))
 means.se <-tapply(data_cut$bpt, t.breaks, semean)
-points(seq(0.05, 0.95, by=0.1), means, pch=16, col="orange")
-segments(seq(0.05, 0.95, by=0.1), means-2*means.se,
-         seq(0.05, 0.95,by=0.1), means+2*means.se,lwd=2, col="orange")
-
-
-gdata<-data.frame(fit=fitted(mod),obs=data_cut$bpt)
-xrange<-range(fitted(mod))
-
+y<-data_cut$bpt
+#points(seq(0.05, 0.95, by=0.1), means, pch=16, col="orange")
+#segments(seq(0.05, 0.95, by=0.1), means-2*means.se,
+#         seq(0.05, 0.95,by=0.1), means+2*means.se,lwd=2, col="orange")
+#semean <-function(x) sd(x)/sqrt(length(x))
+gdata<-data.frame(fit=fitted(mod1),obs=y)
+xrange<-range(fitted(mod1))
 bined<-data.frame(x=seq(0.05, 0.95, by=0.1),y=means)
-
-
 ggplot(aes(x=fit,y=obs),data=gdata)+geom_point(colour="#00CED1",size=3,position = position_jitter (h = 0.05))+
   geom_point(aes(x=x,y=y),size=3,data=bined,colour="#de2d26")+
   geom_errorbar(data=bined,guide="none",aes(x=x,y=y,ymin=y-2*means.se,ymax=y+2*means.se),alpha=0.7,
@@ -47,6 +42,33 @@ ggplot(aes(x=fit,y=obs),data=gdata)+geom_point(colour="#00CED1",size=3,position 
 
 
 
+t.breaks <-cut(fitted(mod2), seq(0,1, by=0.1))
+means <-tapply(data_cut$bpt, t.breaks, mean)
+semean <-function(x) sd(x)/sqrt(length(x))
+means.se <-tapply(data_cut$bpt, t.breaks, semean)
+y<-data_cut$bpt
+gdata2<-data.frame(fit=fitted(mod2),obs=y)
+xrange<-range(fitted(mod2))
+bined<-data.frame(x=seq(0.05, 0.95, by=0.1),y=means)
+ggplot(aes(x=fit,y=obs),data=gdata2)+geom_point(colour="#00CED1",size=3,position = position_jitter (height = 0.05))+
+  geom_point(aes(x=x,y=y),size=3,data=bined,colour="#de2d26")+
+  geom_errorbar(data=bined,guide="none",aes(x=x,y=y,ymin=y-2*means.se,ymax=y+2*means.se),alpha=0.7,
+                colour="#de2d26",width=0.005)+
+  geom_abline(size=1.5,intercept = 0, slope = 1,linetype="dashed",colour="gray65")+coord_cartesian(xlim=c(0.75*xrange[1],1.1*xrange[2]))+
+  theme_bw()+theme(legend.position="top",plot.title = element_text(hjust=0.5),
+                   axis.title.y=element_text(vjust=0.75),
+                   axis.title.x=element_text(vjust=-0.25),
+                   text = element_text(size=25))+xlab("Predicted  AGN fraction")+
+  ylab("Observed AGN Fraction")
+
+
+#AIC
+
+
+#BIC
+
+
+
 
 
 
@@ -56,20 +78,3 @@ ggplot(aes(x=fit,y=obs),data=gdata)+geom_point(colour="#00CED1",size=3,position 
 
 plot(Rsq.budworm, "ROC")
 
-
-
-fit <- glm(bpt ~ logM200_L+RprojLW_Rvir ,
-             data_cut,
-             family = binomial("logit"))
-sjp.glm(fit)
-
-sjp.glm(fit, type = "qq")
-
-m1_lme4 <- lmer( Reaction ~ Days + (Days | Subject), sleepstudy, REML=FALSE )
-
-# construct subject index --- glmer2stan forces you to manage your own cluster indices
-sleepstudy$subject_index <- as.integer(as.factor(sleepstudy$Subject))
-
-# fit with glmer2stan
-m1_g2s <- lmer2stan( Reaction ~ Days + (Days | subject_index), data=sleepstudy )
-m1_g2s@stanmodel
