63850dbfcb2cc3834797a6709281f885374dcf8c
Rafael S. de Souza <rafael.2706@gmail.com>
2015-12-11 17:44:34 +0100

    exploratory

--- script/scatter_3D.R
+++ script/scatter_3D.R
@@ -1,5 +1,8 @@
 #scatter 3D
 require(scatterplot3d)
+require(plyr)
+require(plot3D)
+
 
 # Original--------------------
 # Read and format data
@@ -24,26 +27,64 @@ data_n     <- data3
 #data_n2    <- subset(data_n, zoo=="E" | zoo == "S")
 dataEoriginal<-subset(data_n, zoo=="E")
 dataSoriginal<-subset(data_n, zoo == "S")
+dataEoriginal$color<-rep("red",nrow(dataEoriginal))
+dataSoriginal$color<-rep("cyan",nrow(dataSoriginal))
 
-unmatched<-data_n2[,c("bpt" ,"lgm_tot_p50","sfr_tot_p50","color_gr","zoo")]
+dataEoriginal$PSM<-rep("Before",nrow(dataEoriginal))
+dataSoriginal$PSM<-rep("Before",nrow(dataSoriginal))
+
+dataoriginal<-rbind(dataEoriginal,dataSoriginal)
+
+
+dataraw<-dataoriginal[,c("bpt" ,"lgm_tot_p50","sfr_tot_p50","color_gr","zoo","color","PSM")]
 #-------------------------
-unmatched
+
 
 
 dataE     <- read.table("..//data/matched_E_original.txt",header=TRUE,na.strings="")
 dataS     <- read.table("..//data/matched_S_original.txt",header=TRUE,na.strings="")
-dataE$color<-rep("red2",nrow(dataE))
-dataS$color<-rep("cyan3",nrow(dataS))
+dataE$color<-rep("red4",nrow(dataE))
+dataS$color<-rep("cyan4",nrow(dataS))
+
+dataS$PSM<-rep("After",nrow(dataS))
+dataE$PSM<-rep("After",nrow(dataE))
+
 datam<-rbind(dataE,dataS)
-
-x<-datam$color_gr
-y<-datam$lgm_tot_p50
-z<-datam$sfr_tot_p50
-with(datam,s3d<-{scatterplot3d(x, y, z,color=datam$color, col.axis="blue",
-              col.grid="lightblue", pch=20)})
+datam<-datam[,c("bpt" ,"lgm_tot_p50","sfr_tot_p50","color_gr","zoo","color","PSM")]
 
 
+Full_data<-rbind(dataraw,datam)
+gdata<-melt(Full_data,id=c("bpt","zoo","color","PSM"))
 
 
+ggplot <- function(...) ggplot2::ggplot(...) + scale_color_manual(values=c("#00CED1","#00CED1","#00CED1","#00CED1"))
+unlockBinding("ggplot",parent.env(asNamespace("GGally")))
+assign("ggplot",ggplot,parent.env(asNamespace("GGally")))
+require(GGally)
+gp<-ggpairs(
+  Full_data,columns=2:4,
+  upper = list(continuous = "density", combo = "box"),
+  lower = list(continuous = "points", combo = "dot"),
+  color = "color ",
+  alpha=0.7,
+   title = ""
+)
+
+gp+theme_bw()
+
+ggplot(gdata,aes(y=value,x=PSM))+geom_boxplot()+
+  facet_wrap(zoo~variable,scales="free")
 
 
+x<-Full_data$lgm_tot_p50
+y<-Full_data$color_gr
+z<-Full_data$sfr_tot_p50
+with(Full_data,s3d<-{scatterplot3d(x, y, z,color=Full_data$color, col.axis="gray30",
+              col.grid="gray70", pch=17)})
+
+
+Full_data$color<-as.factor(Full_data$color)
+
+scatter3d(sfr_tot_p50 ~ lgm_tot_p50 + color_gr | color, surface=FALSE, 
+          ellipsoid=TRUE, revolutions=3, data=Full_data)
+scatter3D(x, y, z,col=Full_data$color,colkey = FALSE,theta=-280)
