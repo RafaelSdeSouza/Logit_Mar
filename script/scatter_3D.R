c6650c6ee23c3fab444bef409d8f88530d9854f3
Rafael <rafael.2706@gmail.com>
2015-12-13 10:23:05 +0100

    plot

--- script/scatter_3D.R
+++ script/scatter_3D.R
@@ -1,8 +1,9 @@
 #scatter 3D
-require(scatterplot3d)
-require(plyr)
-require(plot3D)
 
+require(plyr)
+require(reshape2)
+require(ggplot2)
+require(ggthemes)
 
 # Original--------------------
 # Read and format data
@@ -27,8 +28,8 @@ data_n     <- data3
 #data_n2    <- subset(data_n, zoo=="E" | zoo == "S")
 dataEoriginal<-subset(data_n, zoo=="E")
 dataSoriginal<-subset(data_n, zoo == "S")
-dataEoriginal$color<-rep("red",nrow(dataEoriginal))
-dataSoriginal$color<-rep("cyan",nrow(dataSoriginal))
+dataEoriginal$type<-rep("E_full",nrow(dataEoriginal))
+dataSoriginal$type<-rep("S_full",nrow(dataSoriginal))
 
 dataEoriginal$PSM<-rep("Before",nrow(dataEoriginal))
 dataSoriginal$PSM<-rep("Before",nrow(dataSoriginal))
@@ -36,25 +37,108 @@ dataSoriginal$PSM<-rep("Before",nrow(dataSoriginal))
 dataoriginal<-rbind(dataEoriginal,dataSoriginal)
 
 
-dataraw<-dataoriginal[,c("bpt" ,"lgm_tot_p50","sfr_tot_p50","color_gr","zoo","color","PSM")]
+dataraw<-dataoriginal[,c("bpt" ,"lgm_tot_p50","sfr_tot_p50","color_gr","zoo","type","PSM")]
 #-------------------------
 
 
 
 dataE     <- read.table("..//data/matched_E_original.txt",header=TRUE,na.strings="")
 dataS     <- read.table("..//data/matched_S_original.txt",header=TRUE,na.strings="")
-dataE$color<-rep("red4",nrow(dataE))
-dataS$color<-rep("cyan4",nrow(dataS))
+dataE$type<-rep("E_cut",nrow(dataE))
+dataS$type<-rep("S_cut",nrow(dataS))
 
 dataS$PSM<-rep("After",nrow(dataS))
 dataE$PSM<-rep("After",nrow(dataE))
 
 datam<-rbind(dataE,dataS)
-datam<-datam[,c("bpt" ,"lgm_tot_p50","sfr_tot_p50","color_gr","zoo","color","PSM")]
+datam<-datam[,c("bpt" ,"lgm_tot_p50","sfr_tot_p50","color_gr","zoo","type","PSM")]
 
 
 Full_data<-rbind(dataraw,datam)
-gdata<-melt(Full_data,id=c("bpt","zoo","color","PSM"))
+
+
+Full_data$type<-as.factor(Full_data$type)
+ggplot(Full_data,aes(y=lgm_tot_p50,x=sfr_tot_p50,color=type,linetype=type,shape=PSM))+geom_point(size=1.05,alpba=0.6)+
+  geom_density2d(size=0.95,n = 200,aes(alpha=0.95))+
+   scale_color_manual(name="",values=c("red2","salmon2", "royalblue3","cyan4"))+
+  scale_linetype_manual(name="",values=c("solid", "dashed","solid","dashed"))+
+  theme_bw()+
+  theme(legend.position="none",plot.title = element_text(hjust=0.5),
+        axis.title.y=element_text(vjust=0.75),axis.text.x=element_text(size=25),
+        axis.text.y=element_text(size=25),
+        strip.text.x=element_text(size=25),
+        axis.title.x=element_text(vjust=-0.25),
+        text = element_text(size=25),axis.title.x=element_text(size=rel(1)))+
+  coord_cartesian(ylim=c(9.25,12.25))+xlab(expression(log~SFR~(M['\u0298']*yr^{-1})))+ylab(expression(log~M["*"]~(M['\u0298'])))
+
+
+quartz.save(type = 'pdf', file = '..//figures/MgalSFR.pdf',width = 9.5, height = 9)
+
+
+
+
+ggplot(Full_data,aes(y=color_gr,x=sfr_tot_p50,color=type,linetype=type,shape=PSM))+geom_point(size=1.05,alpba=0.7)+
+  geom_density2d(size=0.95,n = 200,aes(alpha=0.95))+
+  scale_color_manual(name="",values=c("red2","salmon2", "royalblue3","cyan4"))+
+  scale_linetype_manual(name="",values=c("solid", "dashed","solid","dashed"))+
+  theme_bw()+
+  theme(legend.position="none",plot.title = element_text(hjust=0.5),
+        axis.title.y=element_text(vjust=0.75),axis.text.x=element_text(size=25),
+        axis.text.y=element_text(size=25),
+        strip.text.x=element_text(size=25),
+        axis.title.x=element_text(vjust=-0.25),
+        text = element_text(size=25),axis.title.x=element_text(size=rel(1)))+
+  coord_cartesian(ylim=c(0,1.5))+xlab(expression(log~SFR~(M['\u0298']*yr^{-1})))+ylab("g-r")
+
+quartz.save(type = 'pdf', file = '..//figures/grSFR.pdf',width = 9.5, height = 9)
+
+
+
+
+
+ggplot(Full_data,aes(y=color_gr,x=lgm_tot_p50,linetype=type,color=type,shape=PSM))+
+  #geom_point(size=1.05,alpba=0.7)+
+  geom_density2d(size=0.95,n = 200,aes(alpha=0.95))+
+  scale_color_manual(name="",values=c("red2","salmon2", "royalblue3","cyan4"))+
+  scale_linetype_manual(name="",values=c("solid", "dashed","solid","dashed"))+
+  theme_bw()+
+  theme(legend.position="none",plot.title = element_text(hjust=0.5),
+        axis.title.y=element_text(vjust=0.75),axis.text.x=element_text(size=25),
+        axis.text.y=element_text(size=25),
+        strip.text.x=element_text(size=25),
+        axis.title.x=element_text(vjust=-0.25),
+        text = element_text(size=25),axis.title.x=element_text(size=rel(1)))+
+  coord_cartesian(ylim=c(0,1.5),xlim=c(9,12))+xlab(expression(log~M["*"]~(M['\u0298'])))+ylab("g-r")
+
+quartz.save(type = 'pdf', file = '..//figures/grMgal.pdf',width = 9.5, height = 9)
+
+
+
+
+# R proj and Mhalo
+
+datag<-rbind(dataE,dataS)
+
+datag2<-subset(datag,type=="E_cut" | type =="S_cut")
+
+ggplot(datag2,aes(y=lgm_tot_p50,x=RprojLW_Rvir,linetype=type,color=type,shape=type))+
+  geom_point(size=2.25,alpba=0.8)+
+  geom_density2d(n = 200,size=1.25)+
+  scale_color_manual(name="",values=c("red3", "royalblue3"))+
+  scale_linetype_manual(name="",values=c("solid", "dashed"))+
+  scale_shape_cleveland()+
+  theme_bw()+
+  theme(legend.position="none",plot.title = element_text(hjust=0.5),
+        axis.title.y=element_text(vjust=0.75),axis.text.x=element_text(size=25),
+        axis.text.y=element_text(size=25),
+        strip.text.x=element_text(size=25),
+        axis.title.x=element_text(vjust=-0.25),
+        text = element_text(size=25),axis.title.x=element_text(size=rel(1)))+
+ylab(expression(log~M["*"]~(M['\u0298'])))+xlab(expression(R/R[vir]))+
+  coord_cartesian(ylim=c(10,12))
+quartz.save(type = 'pdf', file = '..//figures/RMgal.pdf',width = 9.5, height = 9)
+
+
 
 
 ggplot <- function(...) ggplot2::ggplot(...) + scale_color_manual(values=c("#00CED1","#00CED1","#00CED1","#00CED1"))
