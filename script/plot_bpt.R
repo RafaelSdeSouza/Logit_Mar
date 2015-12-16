fe5eab749fff8909a5783781e7063ddb81ec82ac
Marina Trevisan <trevisan@iap.fr>
2015-12-16 15:35:30 +0100

    script plot_bpt.R now gives some statistics on the number of Seyfert galaxies

--- script/plot_bpt.R
+++ script/plot_bpt.R
@@ -36,6 +36,10 @@ xx.xxS = data$zoo == 'S' & is.na(data$zoo) == F
 xx.xxE = data$zoo == 'E' & is.na(data$zoo) == F
 xx.xxS_sey = xx.xxS & data$bpt == "Seyfert" & is.na(data$bpt) == F
 xx.xxE_sey = xx.xxE & data$bpt == "Seyfert" & is.na(data$bpt) == F
+xx.xxS_bpt = xx.xxS & is.na(data$logO3Hb) == F & is.na(data$logN2Ha) == F 
+xx.xxE_bpt = xx.xxE & is.na(data$logO3Hb) == F & is.na(data$logN2Ha) == F 
+xx.xxS_oii = xx.xxS & is.na(data$logO3O2) == F & is.na(data$logN2Ha) == F 
+xx.xxE_oii = xx.xxE & is.na(data$logO3O2) == F & is.na(data$logN2Ha) == F 
 
 #-----------------------
 # PLOT DEFINITIONS
@@ -149,6 +153,41 @@ plot(data$logN2Ha[xx.xxS], data$logO3O2[xx.xxS], xlim = c(-1, 0.5), ylim = c(-1.
 
 points(data$logN2Ha[xx.xxS_sey], data$logO3O2[xx.xxS_sey], pch = pch1sey, cex = cex1sey, col = col1sey)
 
+xx = data$logN2Ha[xx.xxS_sey]
+yy = data$logO3O2[xx.xxS_sey]
+Ke = 0.48 / (xx - 0.21) + 1.25
+Sey = 0.64 * xx -0.06
+sey_bpt_oxy = (yy > Ke | xx > 0.1) & yy > Sey
+
+xx = data$logN2Ha[xx.xxS]
+yy = data$logO3O2[xx.xxS]
+Ke = 0.48 / (xx - 0.21) + 1.25
+Sey = 0.64 * xx -0.06
+sey_oxy = (yy > Ke | xx > 0.1) & yy > Sey
+
+NS = length(data$logN2Ha[xx.xxS])
+NS_bpt = length(data$logN2Ha[xx.xxS_bpt]) 
+NS_oii = length(data$logN2Ha[xx.xxS_oii])
+NS_bpt_oii = length(data$logN2Ha[xx.xxS_bpt & xx.xxS_oii])
+
+print('----------------- SPIRALS -----------------')
+print(sprintf('%-50s%8s%11s%11s', '', 'N', 'N/NS', 'N/NS_d'))
+print(sprintf('%-50s%8i', '#N SPIRALS: ', NS))
+print(sprintf('%-50s%8i%10.2f%1s', '#N spirals in the BPT diagram: ', NS_bpt, NS_bpt / NS * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s', '#N spirals in the OIII/OII diagram: ', NS_oii, NS_oii / NS * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s', '#N spirals in the BPT AND OIII/OII diagram: ', NS_bpt_oii, NS_bpt_oii / NS * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s%10.2f%1s', '#N Seyfert BPT diagram: ', length(data$logN2Ha[xx.xxS_sey]), 
+            length(data$logN2Ha[xx.xxS_sey]) / NS * 100, '%',
+            length(data$logN2Ha[xx.xxS_sey]) / NS_bpt * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s%10.2f%1s', '#N Seyfert OIII/OII diagram: ', length(data$logN2Ha[xx.xxS_sey][sey_oxy]), 
+            length(data$logN2Ha[xx.xxS_sey][sey_oxy]) / NS * 100, '%',
+            length(data$logN2Ha[xx.xxS_sey][sey_oxy]) / NS_oii * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s%10.2f%1s', '#N Seyfert BPT & OIII/OII diagram: ', length(data$logN2Ha[xx.xxS_sey][sey_bpt_oxy]),
+              length(data$logN2Ha[xx.xxS_sey][sey_bpt_oxy]) / NS * 100, '%',
+              length(data$logN2Ha[xx.xxS_sey][sey_bpt_oxy]) / NS_bpt_oii * 100, '%'))
+
+#points(xx[sey_oxy], yy[sey_oxy], pch = pch1sey, cex = cex1sey, col = col1sey)
+
 axis(1, at = seq(-2, 2, 0.5), labels = T, tcl = -0.6)
 axis(1, at = seq(-2, 2, 0.1), labels = F, tcl = -0.3)
 axis(3, at = seq(-2, 2, 0.5), labels = F, tcl = -0.6)
@@ -186,6 +225,41 @@ plot(data$logN2Ha[xx.xxE], data$logO3O2[xx.xxE], xlim = c(-1, 0.5), ylim = c(-1.
      xaxt = 'n', yaxt = 'n', pch = pch2, cex = cex2, col = col2, lwd = 1)
 
 points(data$logN2Ha[xx.xxE_sey], data$logO3O2[xx.xxE_sey], pch = pch2sey, cex = cex2sey, col = col2sey)
+
+xx = data$logN2Ha[xx.xxE_sey]
+yy = data$logO3O2[xx.xxE_sey]
+Ke = 0.48 / (xx - 0.21) + 1.25
+Sey = 0.64 * xx -0.06
+sey_bpt_oxy = (yy > Ke | xx > 0.1) & yy > Sey
+
+xx = data$logN2Ha[xx.xxE]
+yy = data$logO3O2[xx.xxE]
+Ke = 0.48 / (xx - 0.21) + 1.25
+Sey = 0.64 * xx -0.06
+sey_oxy = (yy > Ke | xx > 0.1) & yy > Sey
+
+NE = length(data$logN2Ha[xx.xxE])
+NE_bpt = length(data$logN2Ha[xx.xxE_bpt]) 
+NE_oii = length(data$logN2Ha[xx.xxE_oii])
+NE_bpt_oii = length(data$logN2Ha[xx.xxE_bpt & xx.xxE_oii])
+
+print('----------------- ELLIPTICALS -----------------')
+print(sprintf('%-50s%8s%11s%11s', '', 'N', 'N/NE', 'N/NE_d'))
+print(sprintf('%-50s%8i', '#N ELLIPTICALS: ', NE))
+print(sprintf('%-50s%8i%10.2f%1s', '#N ellipticals in the BPT diagram: ', NE_bpt, NE_bpt / NE * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s', '#N ellipticals in the OIII/OII diagram: ', NE_oii, NE_oii / NE * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s', '#N ellipticals in the BPT AND OIII/OII diagram: ', NE_bpt_oii, NE_bpt_oii / NE * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s%10.2f%1s', '#N Seyfert BPT diagram: ', length(data$logN2Ha[xx.xxE_sey]), 
+              length(data$logN2Ha[xx.xxE_sey]) / NE * 100, '%',
+              length(data$logN2Ha[xx.xxE_sey]) / NE_bpt * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s%10.2f%1s', '#N Seyfert OIII/OII diagram: ', length(data$logN2Ha[xx.xxE_sey][sey_oxy]), 
+              length(data$logN2Ha[xx.xxE_sey][sey_oxy]) / NE * 100, '%',
+              length(data$logN2Ha[xx.xxE_sey][sey_oxy]) / NE_oii * 100, '%'))
+print(sprintf('%-50s%8i%10.2f%1s%10.2f%1s', '#N Seyfert BPT & OIII/OII diagram: ', length(data$logN2Ha[xx.xxE_sey][sey_bpt_oxy]),
+              length(data$logN2Ha[xx.xxE_sey][sey_bpt_oxy]) / NE * 100, '%',
+              length(data$logN2Ha[xx.xxE_sey][sey_bpt_oxy]) / NE_bpt_oii * 100, '%'))
+
+#points(xx[sey_oxy], yy[sey_oxy], pch = pch1sey, cex = cex1sey, col = col1sey)
 
 axis(1, at = seq(-2, 2, 0.5), labels = T, tcl = -0.6)
 axis(1, at = seq(-2, 2, 0.1), labels = F, tcl = -0.3)
