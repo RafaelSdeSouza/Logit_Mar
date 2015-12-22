84f8861080eabc21b882a65351b2df7bb8b6a11a
Rafael <rafael.2706@gmail.com>
2015-12-22 08:24:48 +0100

    new plots

--- script/Figures.R
+++ script/Figures.R
@@ -5,44 +5,7 @@ require(ggthemes)
 require(Cairo)
 require(plyr)
 
-
-
-# Prepare points ------------------------------------------------------------------------
-dataE     <- read.table("..//data/matched_E_original.txt",header=TRUE,na.strings="")
-dataS     <- read.table("..//data/matched_S_original.txt",header=TRUE,na.strings="")
-
-Mmean <-mean(dataE$logM200_L)
-Msd <-sd(dataE$logM200_L)
-#dataE<-dataE[which(dataE$logM200_L<=Mmean+Msd & dataE$logM200_L >= Mmean - Msd),]
-#dataS<-dataS[which(dataS$logM200_L<=Mmean+Msd & dataS$logM200_L >= Mmean - Msd),]
-
-t.breaks <-cut(dataE$RprojLW_Rvir, breaks=c(0,1.5,3,4.5,6,8))
-means <-tapply(dataE$bpt, t.breaks, mean)
-semean <-function(x) sd(x)/sqrt(length(x))
-#semean <-function(x) sqrt(x*(1-x)/length(x))
-means.se <-tapply(dataE$bpt, t.breaks, semean)
-bins<-levels(t.breaks)
-
-
-gdata<-data.frame(x=bins,y=means)
-gdata$gal<-rep("E",nrow(gdata))
-
-
-t.breaks2 <-cut(dataS$RprojLW_Rvir, breaks=c(0,1.5,3,4.5,6,8))
-means2 <-tapply(dataS$bpt, t.breaks2, mean)
-semean2 <-function(x) sd(x)/sqrt(length(x))
-#semean2 <-function(x) sqrt(x*(1-x)/length(x))
-means.se2 <-tapply(dataS$bpt, t.breaks2, semean2)
-
-gdata2<-data.frame(x=bins,y=means2)
-gdata2$gal<-rep("S",nrow(gdata2))
-
-gdata$xc<-c(0.75,2.25,3.75,5.25,7)
-gdata2$xc<-c(0.8,2.5,4,5.5,7.25)
-
 ##------------------------------------------------------------------
-
-
 gMx_S<-read.table("..//data/gMx_S.dat",header=TRUE)
 gMx_E<-read.table("..//data/gMx_E.dat",header=TRUE)
 #gMx_E$x<-gMx_S$x
@@ -57,37 +20,52 @@ gRx_E$x_o<-gRx_E$x*2.061402+2.180115
 
 
 
+# Prepare points ------------------------------------------------------------------------
+dataE     <- read.table("..//data/matched_E_original.txt",header=TRUE,na.strings="")
+dataS     <- read.table("..//data/matched_S_original.txt",header=TRUE,na.strings="")
+
+MmeanE <-mean(dataE$logM200_L)
+MsdE <-sd(dataE$logM200_L)
+MmeanS <-mean(dataS$logM200_L)
+MsdS <-sd(dataS$logM200_L)
+
+dataE_R<-dataE[which(dataE$logM200_L<=MmeanE+MsdE & dataE$logM200_L >= MmeanE - MsdE),]
+dataS_R<-dataS[which(dataS$logM200_L<=MmeanS+MsdS & dataS$logM200_L >= MmeanS - MsdS),]
+
+t.breaks <-cut(dataE_R$RprojLW_Rvir, breaks=c(0,1.5,3,4.5,6,8))
+means <-tapply(dataE_R$bpt, t.breaks, mean)
+semean <-function(x) sd(x)/sqrt(length(x))
+#semean <-function(x) sqrt(x*(1-x)/length(x))
+means.se <-tapply(dataE_R$bpt, t.breaks, semean)
+bins<-levels(t.breaks)
+
+
+gdata<-data.frame(x=bins,y=means)
+gdata$gal<-rep("E",nrow(gdata))
+
+
+t.breaks2 <-cut(dataS_R$RprojLW_Rvir, breaks=c(0,1.5,3,4.5,6,8))
+means2 <-tapply(dataS_R$bpt, t.breaks2, mean)
+semean2 <-function(x) sd(x)/sqrt(length(x))
+#semean2 <-function(x) sqrt(x*(1-x)/length(x))
+means.se2 <-tapply(dataS_R$bpt, t.breaks2, semean2)
+
+gdata2<-data.frame(x=bins,y=means2)
+gdata2$gal<-rep("S",nrow(gdata2))
+
+gdata$xc<-c(0.75,2.25,3.75,5.25,7)
+gdata2$xc<-c(0.8,2.5,4,5.5,7.25)
+
+
+
+
+
 
 #gRx_E$x<-gRx_S$x
 
 
 
 
-#b) M_halo
-#PMx<-
-  ggplot(aes(x=x_o,y=mean),data=gMx_S)+
-    geom_ribbon(data=gMx_E,aes(x=x_o,y=mean,ymin=lwr2, ymax=upr2), alpha=0.9, fill=c("#fcbba1")) +
-    geom_ribbon(data=gMx_E,aes(x=x_o,y=mean,ymin=lwr1, ymax=upr1), alpha=0.8, fill=c("#de2d26")) +
-    geom_line(aes(x=x_o,y=mean),data=gMx_E,size=1,linetype="dotted")+
-  geom_ribbon(data=gMx_S,aes(x=x_o,y=mean,ymin=lwr2, ymax=upr2),alpha=0.6,  fill=c("#E0FFFF")) +
-  geom_ribbon(data=gMx_S,aes(x=x_o,y=mean,ymin=lwr1, ymax=upr1),alpha=0.5,  fill=c("#00CED1")) +
-  geom_line(size=1,linetype="dashed")+
-   
-  theme_bw()+
-  theme(legend.position="none",plot.title = element_text(hjust=0.5),
-        axis.title.y=element_text(vjust=0.75),axis.text.x=element_text(size=25),
-        axis.text.y=element_text(size=25),
-        strip.text.x=element_text(size=25),
-        axis.title.x=element_text(vjust=-0.25),
-        text = element_text(size=25),axis.title.x=element_text(size=rel(1)))+
-#  xlab(expression(log~M[halo]/M['\u0298']))+
-  xlab(expression(log~M[halo]))+
-  ylab(expression(P[AGN]))+coord_cartesian(ylim=c(0.15,0.85))
-quartz.save(type = 'pdf', file = '..//figures/P_Mx.pdf',width = 9.5, height = 9)
-
-#cairo_pdf("..//figures/P_Mx.pdf",width = 9.25, height = 9)
-#PMx
-#dev.off()
 
 #c) R_proj
 #PRx<-
@@ -102,7 +80,7 @@ quartz.save(type = 'pdf', file = '..//figures/P_Mx.pdf',width = 9.5, height = 9)
     geom_errorbar(data=gdata,guide="none",aes(x=xc,y=y,ymin=y-2*means.se,ymax=y+2*means.se),
                   colour="red3",width=0.05)+
     geom_point(aes(x=xc,y=y),size=3,data=gdata2,colour="cyan3")+
-    geom_errorbar(data=gdata2,guide="none",aes(x=xc,y=y,ymin=y-2*means.se,ymax=y+2*means.se),
+    geom_errorbar(data=gdata2,guide="none",aes(x=xc,y=y,ymin=y-1.96*means.se2,ymax=y+1.96*means.se2),
                   colour="cyan3",width=0.05)+
    theme_bw()+
   theme(legend.position="none",plot.title = element_text(hjust=0.5),
@@ -122,6 +100,79 @@ quartz.save(type = 'pdf', file = '..//figures/P_Rx.pdf',width = 9.5, height = 9)
 #CairoPDF("..//figures/P_Rx.pdf",width = 9.25, height = 9)
 #PRx
 #dev.off()
+
+
+
+
+##------------------------------------------------------------------
+RmeanE <-mean(dataE$RprojLW_Rvir)
+RsdE <-sd(dataE$RprojLW_Rvir)
+RmeanS <-mean(dataS$RprojLW_Rvir)
+RsdS <-sd(dataS$RprojLW_Rvir)
+
+dataE_M<-dataE[which(dataE$RprojLW_Rvir<=RmeanE+RsdE & dataE$RprojLW_Rvir >= RmeanE - RsdE),]
+dataS_M<-dataS[which(dataS$RprojLW_Rvir<=RmeanS+RsdS & dataS$RprojLW_Rvir >= RmeanS - RsdS),]
+
+t.breaks <-cut(dataE_M$logM200_L, breaks=c(13,13.3,13.6,13.9,14.2,14.6))
+means <-tapply(dataE_M$bpt, t.breaks, mean)
+semean <-function(x) sd(x)/sqrt(length(x))
+#semean <-function(x) sqrt(x*(1-x)/length(x))
+means.se <-tapply(dataE_M$bpt, t.breaks, semean)
+bins<-levels(t.breaks)
+
+
+gdata<-data.frame(x=bins,y=means)
+gdata$gal<-rep("E",nrow(gdata))
+
+
+t.breaks2 <-cut(dataS_M$logM200_L, breaks=c(13,13.3,13.6,13.9,14.2,14.6))
+means2 <-tapply(dataS_M$bpt, t.breaks2, mean)
+semean2 <-function(x) sd(x)/sqrt(length(x))
+#semean2 <-function(x) sqrt(x*(1-x)/length(x))
+means.se2 <-tapply(dataS_M$bpt, t.breaks2, semean2)
+
+gdata2<-data.frame(x=bins,y=means2)
+gdata2$gal<-rep("S",nrow(gdata2))
+
+gdata$xc<-c(13.2,13.5,13.8,14.1,14.4)
+gdata2$xc<-c(13.225,13.525,13.825,14.125,14.425)
+
+
+
+##------------------------------------------------------------------
+#b) M_halo
+#PMx<-
+ggplot(aes(x=x_o,y=mean),data=gMx_S)+
+  geom_ribbon(data=gMx_E,aes(x=x_o,y=mean,ymin=lwr2, ymax=upr2), alpha=0.9, fill=c("#fcbba1")) +
+  geom_ribbon(data=gMx_E,aes(x=x_o,y=mean,ymin=lwr1, ymax=upr1), alpha=0.8, fill=c("#de2d26")) +
+  geom_line(aes(x=x_o,y=mean),data=gMx_E,size=1,linetype="dotted")+
+  geom_ribbon(data=gMx_S,aes(x=x_o,y=mean,ymin=lwr2, ymax=upr2),alpha=0.6,  fill=c("#E0FFFF")) +
+  geom_ribbon(data=gMx_S,aes(x=x_o,y=mean,ymin=lwr1, ymax=upr1),alpha=0.5,  fill=c("#00CED1")) +
+  geom_line(size=1,linetype="dashed")+
+  geom_point(aes(x=xc,y=y),size=3,data=gdata,colour="red3")+
+  geom_errorbar(data=gdata,guide="none",aes(x=xc,y=y,ymin=y-1.96*means.se,ymax=y+1.96*means.se),
+                colour="red3",width=0.05)+
+  geom_point(aes(x=xc,y=y),size=3,data=gdata2,colour="cyan3")+
+  geom_errorbar(data=gdata2,guide="none",aes(x=xc,y=y,ymin=abs(y-1.96*means.se2),ymax=y+1.96*means.se2),
+                colour="cyan3",width=0.05)+
+  theme_bw()+
+  theme(legend.position="none",plot.title = element_text(hjust=0.5),
+        axis.title.y=element_text(vjust=0.75),axis.text.x=element_text(size=25),
+        axis.text.y=element_text(size=25),
+        strip.text.x=element_text(size=25),
+        axis.title.x=element_text(vjust=-0.25),
+        text = element_text(size=25),axis.title.x=element_text(size=rel(1)))+
+  #  xlab(expression(log~M[halo]/M['\u0298']))+
+  xlab(expression(log~M[halo]))+
+  ylab(expression(P[AGN]))+coord_cartesian(xlim=c(13.3,14.7),ylim=c(0.025,1))
+
+
+quartz.save(type = 'pdf', file = '..//figures/P_Mx.pdf',width = 9.5, height = 9)
+
+#cairo_pdf("..//figures/P_Mx.pdf",width = 9.25, height = 9)
+#PMx
+#dev.off()
+
 
 
 
