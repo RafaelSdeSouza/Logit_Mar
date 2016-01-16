c50658d752533cc9cb2928dcafffe12f98323246
Rafael S. de Souza <rafael.2706@gmail.com>
2016-01-16 19:25:00 +0100

    new plot

--- script/plot_bpt_RSS.R
+++ script/plot_bpt_RSS.R
@@ -58,15 +58,15 @@ xx.xxE_bpt = xx.xxE & is.na(data$logO3Hb) == F & is.na(data$logN2Ha) == F
 # PLOT DEFINITIONS
 #-----------------------
 cc = 0.0
-pch1 = 18; cex1 = 0.3; col1 = rgb(cc, cc, cc, 0.8)
-pch2 = 18; cex2 = 0.3; col2 = rgb(cc, cc, cc, 0.8)
-pch1sey = 18; cex1sey = 1; col1sey = rgb(0.9, 0, 0, 0.8)
-pch2sey = 18; cex2sey = 1; col2sey = rgb(0.9, 0, 0, 0.8)
+pch1 = 18; cex1 = 0.3; col1 = "grey"
+pch2 = 18; cex2 = 0.3; col2 = "grey"
+pch1sey = 20; cex1sey = 1; col1sey = "orange2"
+pch2sey = 20; cex2sey = 1; col2sey = "orange2"
 
 #-----------------------
 # OPEN FIGURE FILE
 #-----------------------
-pdf(output_file, width = 13, height = 8)
+pdf(output_file, width = 13, height = 6.5)
 par(mfrow = c(1, 2), mar = c(5, 5, 2, 2), cex.lab = 1.5, cex.axis = 1.5)
 
 #-----------------------
@@ -77,7 +77,7 @@ plot(data$logN2Ha[xx.xxS], data$logO3Hb[xx.xxS], xlim = c(-1, 0.5), ylim = c(-1.
      ylab = expression(paste('log ([OIII]/H', beta, ')')),
      xaxt = 'n', yaxt = 'n', pch = pch1, cex = cex1, col = col1, lwd = 1.5)
 points(data$logN2Ha[xx.xxS_sey], data$logO3Hb[xx.xxS_sey], pch = pch1sey, cex = cex1sey, col = col1sey)
-points(data$logN2Ha[xx.xxS_SF], data$logO3Hb[xx.xxS_SF], pch = 20, cex = 1, col = "cyan3")
+points(data$logN2Ha[xx.xxS_SF], data$logO3Hb[xx.xxS_SF], pch = 20, cex = 1, col = "deepskyblue2")
 
 axis(1, at = seq(-2, 2, 0.5), labels = T, tcl = -0.6)
 axis(1, at = seq(-2, 2, 0.1), labels = F, tcl = -0.3)
@@ -95,17 +95,19 @@ legend(-1, 1.5, c(leg1), bty = 'n', pt.cex = c(cex1), pch = c(pch1),
        col = c(col1), cex = 1.2)
 par(font = 1)
 
+
+
 xx = seq(-4, 0.0, 0.01)
-#Ka = 0.61 / (xx - 0.05) + 1.30
 Ka = 0.61 / (xx - 0.05) + 1.30
-#Ka = 0.33 / (xx +0.11) + 1.01
-lines(xx, Ka, col = 'red', lwd = 1.5)
+lines(xx, Ka, col = 'grey30', lwd = 3,lty = 1)
 
 xx = seq(-4, 0.4, 0.01)
 Ke = 0.61 / (xx - 0.47) + 1.19
-lines(xx, Ke, col = 'darkgreen', lwd = 1.5)
+lines(xx, Ke, col = 'grey30', lwd = 3,lty = 4)
 
-
+xx = seq(-0.43, 5, 0.01)
+Sey = 1.01 * xx + 0.48
+lines(xx, Sey, col = 'grey30', lwd = 3, lty = 2)
 
 #xx = seq(-4, 0.4, 0.01)
 #Gra=(-30.787 +1.1358*xx + 0.27297*xx^2)*tanh(5.7409*xx)- 31.093
@@ -113,15 +115,16 @@ lines(xx, Ke, col = 'darkgreen', lwd = 1.5)
 
 
 
-xx = seq(-0.43, 5, 0.01)
-Sey = 1.01 * xx + 0.48
-lines(xx, Sey, col = 'black', lwd = 2, lty = 2)
 
-legend(-0.7, -1.2, 'Kauffmann+03', bty = 'n', text.col = 'red')
-legend(0.15, -1.2, 'Kewley+01', bty = 'n', text.col = 'darkgreen')
-legend(0.15, 0.7, 'Kewley+06', bty = 'n', text.col = 'black')
+
 
 par(font = 2)
+legend(-0.7, -1.2, 'Kauffmann+03', bty = 'n', text.col = 'grey30')
+legend(-1.15, 0.2, 'Star Forming', bty = 'n',cex = 1.2, text.col = 'deepskyblue3')
+legend(0.15, -1.2, 'Kewley+01', bty = 'n', text.col = 'grey30')
+legend(0.15, -0.25, 'Liner',cex = 1.2, bty = 'n', text.col = 'black')
+legend(0.15, 0.7, 'Kewley+06', bty = 'n', text.col = 'grey30')
+legend(-0.8, 1, 'Seyfert', bty = 'n', cex = 1.2, text.col = 'orange3')
 legend(-0.28, -1.2, 'Composite', bty = 'n', cex = 1.2)
 par(font = 1)
 
@@ -133,7 +136,7 @@ plot(data$logN2Ha[xx.xxE], data$logO3Hb[xx.xxE], xlim = c(-1, 0.5), ylim = c(-1.
      ylab = expression(paste('log ([OIII]/H', beta, ')')),
      xaxt = 'n', yaxt = 'n', pch = pch2, cex = cex2, col = col2, lwd = 1)
 points(data$logN2Ha[xx.xxE_sey], data$logO3Hb[xx.xxE_sey], pch = pch2sey, cex = cex2sey, col = col2sey)
-points(data$logN2Ha[xx.xxE_SF], data$logO3Hb[xx.xxE_SF], pch = 20, cex = 1, col = "cyan3")
+points(data$logN2Ha[xx.xxE_SF], data$logO3Hb[xx.xxE_SF], pch = 20, cex = 1, col = "deepskyblue2")
 
 axis(1, at = seq(-2, 2, 0.5), labels = T, tcl = -0.6)
 axis(1, at = seq(-2, 2, 0.1), labels = F, tcl = -0.3)
@@ -153,26 +156,25 @@ par(font = 1)
 
 xx = seq(-4, 0.0, 0.01)
 Ka = 0.61 / (xx - 0.05) + 1.30
-lines(xx, Ka, col = 'red', lwd = 1.5)
+lines(xx, Ka, col = 'grey30', lwd = 3,lty = 1)
 
 xx = seq(-4, 0.4, 0.01)
 Ke = 0.61 / (xx - 0.47) + 1.19
-lines(xx, Ke, col = 'darkgreen', lwd = 1.5)
+lines(xx, Ke, col = 'grey30', lwd = 3,lty = 4)
 
 xx = seq(-0.43, 5, 0.01)
 Sey = 1.01 * xx + 0.48
-lines(xx, Sey, col = 'black', lwd = 2, lty = 2)
-
-legend(-0.7, -1.2, 'Kauffmann+03', bty = 'n', text.col = 'red')
-legend(0.15, -1.2, 'Kewley+01', bty = 'n', text.col = 'darkgreen')
-legend(0.15, 0.7, 'Kewley+06', bty = 'n', text.col = 'black')
+lines(xx, Sey, col = 'grey30', lwd = 3, lty = 2)
 
 par(font = 2)
+legend(-0.7, -1.2, 'Kauffmann+03', bty = 'n', text.col = 'grey30')
+legend(-1.15, 0.2, 'Star Forming', bty = 'n',cex = 1.2, text.col = 'deepskyblue3')
+legend(0.15, -1.2, 'Kewley+01', bty = 'n', text.col = 'grey30')
+legend(0.15, -0.25, 'Liner',cex = 1.2, bty = 'n', text.col = 'black')
+legend(0.15, 0.7, 'Kewley+06', bty = 'n', text.col = 'grey30')
+legend(-0.8, 1, 'Seyfert', bty = 'n', cex = 1.2, text.col = 'orange3')
 legend(-0.28, -1.2, 'Composite', bty = 'n', cex = 1.2)
 par(font = 1)
-
-
-
 
 
 dev.off()
