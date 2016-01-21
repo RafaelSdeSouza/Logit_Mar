5a199d66a89c5653fd6531be548d25a653a05a42
Rafael <rafael.2706@gmail.com>
2016-01-21 10:43:29 +0100

    new plot

--- script/scatter_3D.R
+++ script/scatter_3D.R
@@ -98,18 +98,17 @@ quartz.save(type = 'pdf', file = '..//figures/grSFR.pdf',width = 9.5, height = 9
 
 
 
-ggplot(Full_data,aes(y=color_gr,x=lgm_tot_p50,linetype=PSM,group=PSM,colour=color_gr
+ggplot(Full_data,aes(y=color_gr,x=lgm_tot_p50,size=bpt,shape=bpt,linetype=PSM,group=PSM,colour=color_gr
                      ))+
- # geom_point(size=0.8,alpha=0.15,aes(colour=color_gr))+
-#  scale_size_manual(name="",values=c(1,1,1,1))+
+
 #  scale_shape_manual(values=c(15,4))+
-  stat_density2d(aes(alpha=..level.., fill=..level..), size=1, 
-                 geom="polygon")+
-#  scale_color_manual(name="",values=c("red3","green4"))+
+  stat_density2d( size=0.7,colour="grey60")+
+  geom_point(alpha=0.5,aes(colour=bpt))+
+  scale_size_manual(name="",values=c(2.25,0.75))+
+  scale_color_manual(name="",values=c("orange2","deepskyblue2"))+
 #  scale_linetype_manual(name="",values=c("solid", "dashed","solid","dashed"))+
   scale_linetype_stata()+
-  scale_color_gradient()+
-  scale_fill_continuous_tableau()+
+#  scale_color_gradient()+
   theme_bw()+
   theme(legend.position="none",plot.title = element_text(hjust=0.5),
         axis.title.y=element_text(vjust=0.75),axis.text.x=element_text(size=25),
@@ -117,10 +116,10 @@ ggplot(Full_data,aes(y=color_gr,x=lgm_tot_p50,linetype=PSM,group=PSM,colour=colo
         strip.text.x=element_text(size=25),
         axis.title.x=element_text(vjust=-0.25),
         text = element_text(size=25),axis.title.x=element_text(size=rel(1)))+
-  coord_cartesian(ylim=c(0,1.5),xlim=c(9,12))+xlab("Log (stellar mass)")+ylab("(g-r)")+
+  coord_cartesian(ylim=c(0.1,1.25),xlim=c(9.5,12))+xlab(expression(Log(M["*"])))+ylab("(g-r)")+
   facet_wrap(~zoo)
 
-quartz.save(type = 'pdf', file = '..//figures/grMgal.pdf',width = 9.5, height = 9)
+quartz.save(type = 'pdf', file = '..//figures/grMgal.pdf',width = 16, height = 8)
 
 ggplot(datam,aes(y=color_gr,x=lgm_tot_p50,linetype=type,color=zoo))+
   geom_point(aes(alpha=0.25,shape=bpt,size=bpt))+
