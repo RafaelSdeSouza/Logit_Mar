50762bdcb827c252b649a9dfedff1dde09dc55b6
Rafael S. de Souza <rafael.2706@gmail.com>
2016-03-09 20:15:21 +0100

    oi

--- script/JAGS_CODE_Paper.R
+++ script/JAGS_CODE_Paper.R
@@ -1,12 +1,10 @@
-# JAGS code paper
+# JAGS Script
 
 #  Required libraries
-library(rjags);library(MASS);
-library(parallel);library(rjags)
-cl       <- makeCluster(3) # For parallel computing using 3 cores
+library(R2jags)
 
 ############### Data
-data <-read.csv("https://raw.githubusercontent.com/RafaelSdeSouza/Logit_Mar/master/data/Seyfert.csv",header=T)
+data <-read.csv("https://goo.gl/gvFSH8",header=T)
 
 X          <- model.matrix( ~  logM200 + r_r200, data = data) # Predictors
 K          <- ncol(X)                   # Number of Predictors including the intercept 
@@ -25,7 +23,7 @@ jags_data  <- list(Y= y,
 ############### Model
 jags_model<-"model{
 # Shared hyperpriors for beta coefficients
-tau ~ dgamma(0.001,0.001) # Precision
+tau ~ dgamma(1e-3,1e-3) # Precision
 mu  ~ dnorm(0,1e-3) # mean
 
 # Diffuse prior for beta coefficients
@@ -44,18 +42,25 @@ beta[2,gal[i]]*X[i,2]+beta[3,gal[i]]*X[i,3]
 }
 }"
       
-params <- c("beta") # Monitor these parameter  
-inits0  <- function () {list(beta = matrix(rnorm(6,0, 0.01),ncol=2))} # A function to generat initial values for mcmc
-inits1=inits0();inits2=inits0();inits3=inits0()         # Generate initial values for three chains
+params <- c("beta") # Monitor this parameter  
+inits  <- function () {list(beta = matrix(rnorm(6,0, 0.01),ncol=2))} # A function to generate initial values for mcmc
+
 
 # Run mcmc
-bin    = 10^4   # burn-in samples
-ad     = 10^4   # Number of adaptive samples
-s      = 3*10^4 # Number of samples for each chain
-nc     = 3      # Number of mcmc
-th     = 10     # Thinning value
-jags_fit  <- run.jags(method="rjparallel",data = jags_data,inits = list(inits1,inits2,inits3),model=jags_model,
-                        n.chains = nc,adapt=ad,monitor=c(params),burnin=bin,thin=th,sample=s,summarise=FALSE,plots=FALSE)
+burn    = 2*10^4   # burn-in samples
+s       = 5*10^4   # Number of samples for each chain
+nc      = 3        # Number of mcmc
+th      = 10        # Thinning value
+
+# JAGS MCMC
+jags_fit <- jags(data = jags_data,
+                 inits      = inits,
+                 parameters = params,
+                 model.file = textConnection(jags_model),
+                 n.chains   = nc,
+                 n.thin     = th,
+                 n.iter     = s,
+                 n.burnin   = burn)
 
 ############### Output
 print(jags_fit,intervals=c(0.025, 0.975), digits=3)
